d7dc38e4b9b7b4711947ad622751b0af754abb7b
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2021-10-12 16:09:41 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from app.forms import CreatePokemonForm
 
-def root(request):
+def create(request):
     if request.GET:
         form = CreatePokemonForm(request.GET)
         if form.is_valid():
@@ -14,13 +14,15 @@ def root(request):
             type = form['type']
             type2 = form['type2']
             pokemon = [creator, name, type, height, weight, region, description, type, type2]
-            return render(request, "root.html", {"form": form,"pokemon":pokemon})
+            return render(request, "create.html", {"form": form,"pokemon":pokemon})
     else:
         form = CreatePokemonForm()
-    return render(request, "root.html", {"form":form})
+    return render(request, "create.html", {"form":form})
+
 
 def main(request):
     return render(request, "dex.html")
 
+
 def pokemon(request):
-    return render(request, "singe_poke.html")
+    return render(request, "pokemon.html")
