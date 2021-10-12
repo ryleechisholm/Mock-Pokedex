627a53a952e58f6183b4f11c484b003f7ae23bc1
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2021-10-12 11:35:49 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,18 @@
+from django.shortcuts import render
+from app.forms import CreatePokemonForm
 
+def root(request):
+    if request.GET:
+        form = CreatePokemonForm(request.GET)
+        if form.is_valid():
+            creator = form['creator']
+            name = form['name']
+            height = form['height']
+            weight = form['weight']
+            region = form['region']
+            description = form['description']
+            pokemon = [creator, name, type, height, weight, region, description]
+            return render(request, "root.html", {"form": form,"pokemon":pokemon})
+    else:
+        form = CreatePokemonForm()
+    return render(request, "root.html", {"form":form})
