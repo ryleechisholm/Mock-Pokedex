0546c07be621e6ef246e709a3edd86df6aecaeab
Michelle Rankin <89100717+MichelleRankin@users.noreply.github.com>
2021-10-12 15:02:40 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
-TYPES = [('grass','Grass'),
+TYPES = [('--Choose One--',
+    (
+    ('grass','Grass'),
     ('fire','Fire'), 
     ('water','Water'), 
     ('flying','Flying'),
@@ -17,18 +19,18 @@ TYPES = [('grass','Grass'),
     ('ground','Ground'), 
     ('electric','Electric'), 
     ('fairy','Fairy'), 
-    ('ice','Ice'), ]
+    ('ice','Ice'),
+    ('none', "None"),
+    ),)]
+ 
 
 class CreatePokemonForm(forms.Form):
-    Creator = forms.CharField(min_length=0, max_length=100, required=True)
-    name = forms.CharField(min_length=0, max_length=100, required=True)
-    height = forms.IntegerField(min_value=1, max_value=60, required=True)
-    weight = forms.IntegerField(min_value=1, required=True)
-    region = forms.CharField(min_length=0, max_length=500, required=True)
-    description = forms.CharField(min_length=0, max_length=1000, required=True)
-    type = forms.CharField(widget=forms.Select(choices=TYPES))
-    type2 = forms.CharField(widget=forms.Select(choices=TYPES))
-
-
-    
+    Creator = forms.CharField(widget=forms.TextInput(attrs={'placeholder': ' Your Name', 'style': 'width: 300px;'}), min_length=0, max_length=100, required=True, label="What is your Name?")
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your Pokémon Name', 'style': 'width: 300px;'}), min_length=0, max_length=100, required=True, label="What would you like to name your Pokémon?")
+    height = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Pokémon Height', 'style': 'width: 300px;'}), min_value=1, max_value=60, required=True, label="How tall is your Pokémon?")
+    weight = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Pokémon Weight', 'style': 'width: 300px;'}), min_value=1, required=True, label="How much does you Pokémon weight?")
+    region = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Pokémon Birth Region', 'style': 'width: 300px;'}), min_length=0, max_length=500, required=True, label="What region is your Pokémon from?")
+    description = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Pokémon Description', 'style': 'width: 300px;'}), min_length=0, max_length=1000, required=True, label="Describe your Pokémon.")
+    type = forms.CharField(widget=forms.Select(choices=TYPES), label="What type is your Pokémon?")
+    type2 = forms.CharField(widget=forms.Select(choices=TYPES), label="What is your other type?")
 
