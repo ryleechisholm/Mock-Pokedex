4314164142522248217f6d36ea8e5d72706bfb92
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2021-10-12 15:28:08 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 import app.views
 urlpatterns = [
-    path('', app.views.root, name="root"),
+    path('create', app.views.create, name="create"),
+    path('', app.views.main, name="main"),
+    path('pokémon/', app.views.pokemon, name="pokemon")
 ]
