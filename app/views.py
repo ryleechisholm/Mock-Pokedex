156915ab81833de318720512824e506665f0b1eb
dship24 <87671216+dship24@users.noreply.github.com>
2021-10-13 13:49:29 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -19,10 +19,32 @@ def create(request):
         form = CreatePokemonForm()
     return render(request, "create.html", {"form":form})
 
+def dex(request):
+    context = {
+        "Pokemon": {
+            "bulbasaur": "Bulbasaur",
+            "ivysaur": "Ivysaur",
+            "venusaur": "Venusaur",
+            "charmander": "Charmander"}}
+    return render(request, "dex.html", context)
 
-def main(request):
-    return render(request, "dex.html")
+            
+class Gen_1:    
+    def __init__(self, name, height, weight, region, description, type1, type2):
+        self.name = name
+        self.height = height
+        self.weight = weight
+        self.region = region
+        self.description = description
+        self.types = [type1, type2]
 
+def entry(request, pokemon):
+    context = {
+        "pokemon": pokemon,
+        "Pokemon": {
+            "bulbasaur": Gen_1("Bulbasaur", 0.7, 6.9, "Kanto", "Bulbasaur can be seen napping in bright sunlight.  There is a seed on its back.  By soaking up the sun's rays, the seed grows progressively larger.", "Grass", "Poison"),
+            "ivysaur": Gen_1("Ivysaur", 0.5, 0.5, "Kanto", "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.", "Grass", "Poison"),
+            "venusaur": Gen_1("Venusaur", 0.5, 0.5, "Kanto", "Its plant blooms when it is absorbing solar energy.  It stays on the move to seek sunlight.", "Grass", "Poison"),
+            "charmander": Gen_1("Charmander", 0.6, 8.5, "Kanto", "The flame that burns at the tip of its tail is an indication of its emotions.  The flame wavers when Charmander is enjoying itself.  If the pokémon becomes enraged the flame burns fiercely.", "Fire", None),}}
+    return render(request, "entry.html", context)
 
-def pokemon(request):
-    return render(request, "pokemon.html")
