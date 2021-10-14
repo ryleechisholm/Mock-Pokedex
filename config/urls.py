f008e7d07e6527382bc3646155d5892ac566bd62
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2021-10-14 18:13:57 -0500

    Delete config directory

--- config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-import app.views
-
-urlpatterns = [
-    path('create/', app.views.create, name="create"),
-    path('', app.views.dex, name="dex"),
-    path('entry/<pokemon>', app.views.entry, name="entry")
-]
