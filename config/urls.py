49805e6b7c71f42c3ceff08ab3a8b84e9f18846e
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2021-10-15 09:08:22 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,7 @@ import app.views
 urlpatterns = [
     path('create/', app.views.create, name="create"),
     path('', app.views.dex, name="dex"),
-    path('entry/<pokemon>', app.views.entry, name="entry"),
+    path('entry/<pokemon>/', app.views.entry, name="entry"),
+    path('user-entry/<pokemon>/', app.views.user_entry, name="user-entry")
 ]
+     
